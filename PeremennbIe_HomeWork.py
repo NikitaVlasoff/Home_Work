2a77d26c6e9e2c14bfc7911cf8fcaf25cf9a0c61
NikitaVlasoff <42hmb42@gmail.com>
2024-10-28 15:26:47 +0300

    Add files via upload

---
+++ PeremennbIe_HomeWork.py
@@ -0,0 +1,6 @@
+qty_home_work = 12
+qty_hours = 1.5
+well_name = 'Python'
+average_time = (qty_hours / qty_home_work)
+print(average_time, type(average_time))
+print('Курс:',well_name, ',', 'Всего задач:', qty_home_work, ',', 'Затраченно часов:', qty_hours, ',' , 'Среднее время выполнения:', average_time,'.')
